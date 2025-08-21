7ccb99be59659f0276f24553a80c4f52b2f2f07b
karmek-k <33288445+karmek-k@users.noreply.github.com>
2025-08-21 10:22:22 +0200

    Make a base synth

---
+++ synth/base.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from synth import DEFAULT_SAMPLING_RATE
+
+
+class BaseSynth:
+    """Base class for synthesizers."""
+
+    sampling_rate = DEFAULT_SAMPLING_RATE
+
+    def generate(self, length):
+        """Generate a buffer of synthesized samples."""
+
+        buffer = np.zeros(self.sampling_rate * length)
+
+        for sample_idx in range(buffer.shape[0]):
+            t = sample_idx / self.sampling_rate  # sample time in seconds
+            amp = self.function(t)
+
+            buffer[sample_idx] = amp
+
+        return buffer
+
+    def function(self, t):
+        """Synthesis function."""
+
+        return 0.0
--- synth/sine.py
+++ synth/sine.py
@@ -1,21 +1,16 @@
 import numpy as np
 
 from synth import DEFAULT_SAMPLING_RATE
+from synth.base import BaseSynth
 
 
-class SineSynth:
+class SineSynth(BaseSynth):
+    """Synthesizer that generates a constant sine wave."""
+
     sampling_rate = DEFAULT_SAMPLING_RATE
 
     def __init__(self, frequency):
         self.frequency = frequency
 
-    def generate(self, length):
-        buffer = np.zeros(self.sampling_rate * length)
-
-        for sample_idx in range(buffer.shape[0]):
-            t = sample_idx / self.sampling_rate  # sample time in seconds
-            amp = np.sin(2 * np.pi * t * self.frequency)
-
-            buffer[sample_idx] = amp
-
-        return buffer
+    def function(self, t):
+        return np.sin(2 * np.pi * t * self.frequency)
